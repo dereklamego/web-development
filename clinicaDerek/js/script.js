1ec7da606f4ba9eefb439adaf7f815162b425dbd
Derek Lamego <lamego.derek@outlook.com>
2020-11-14 01:48:46 -0300

    Otimização do codigo, com criação de funções

--- clinicaDerek/js/script.js
+++ clinicaDerek/js/script.js
@@ -1,43 +1,51 @@
 var cardsEllement = document.querySelector('.card-container');
 
-function verify(){
-    // var nome = document.getElementById("nome");
-    // var email = document.getElementById("email");
-    // var age = document.getElementById("idade");
-    // var phone = document.getElementById("tel");
+function idadeValida(age){
+    return age >= 40;
+}
+
+function criaElemento(element){
+    return document.createElement(element);
+}
+
+function formValido(){
     if(nome.value =="" || email.value =="" || idade.value =="" || tel.value ==""){
         alert("Dados incompletos")
-    }else if(idade.value>=40){
-        alert("Elegivel para exame!")
+        return false;
     }else{
-        alert("Inelegível para exame. Idade insuficiente!")
+        return true;
     }
 }
 
-function creatBox(){
-    var boxInfo = document.createElement('div');
-    var textp = document.createElement('p');
+function verify(){
+    if(formValido)
+        idadeValida(idade.value)?alert("Elegivel para exame!"):alert("Inelegível para exame. Idade insuficiente!");
+}
 
-    if(nome.value =="" || email.value =="" || idade.value =="" || tel.value ==""){
-        alert("Dados incompletos")
-    }else if(idade.value>=40){
-        textp.innerHTML = "Nome: " + nome.value +  "<br>Idade: " + idade.value+ "<br><br>Situação: Encaminhar para exames de rotina especificos com proctologista."; 
+function creatBox(){
+    var boxInfo = criaElemento('div');
+    var textp = criaElemento('p');
+
+    if(formValido()){
+        if(idadeValida(idade.value)){
+            var condicao = "Situação: Encaminhar para exames de rotina especificos com proctologista."
+            boxInfo.style.backgroundColor = 'rgb(1, 102, 210)';
+        }else{
+            var condicao = "Situação: Não elegivel para realizar exames mais complexos.";
+            boxInfo.style.backgroundColor = 'rgb(0, 139, 46)';
+        }
+        textp.innerHTML = "Nome: " + nome.value +  "<br>Idade: " + idade.value+ "<br><br>"+condicao;
         boxInfo.appendChild(textp);
         cardsEllement.appendChild(boxInfo);
-        boxInfo.style.backgroundColor = 'rgb(1, 102, 210)';
-    }else{
-        textp.innerHTML = "Nome:" + nome.value + "<br>Idade:"+ idade.value+ "<br><br>Situação: Não elegivel para realizar exames mais complexos.";
-        boxInfo.appendChild(textp);
-        cardsEllement.appendChild(boxInfo);
-        boxInfo.style.backgroundColor = 'rgb(0, 139, 46)';
     }
-        
+
     boxInfo.style.width = '295px';
     boxInfo.style.height = '150px';
     boxInfo.style.padding= '0px 10px';
     boxInfo.style.marginTop ='10px';
     boxInfo.style.color ='white';
    
+    clearInput();
 }
 
 function clearInput(){
